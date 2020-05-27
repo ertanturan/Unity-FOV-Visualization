6ac134ae5d0304942119331b6289f2bbacd1a961
Ertan Turan <ertanturan@outlook.com>
2020-05-27 20:41:45 +0300

    setting the base controller

--- Assets/Scripts/FieldOfView.cs
+++ Assets/Scripts/FieldOfView.cs
@@ -2,45 +2,5 @@
 
 public class FieldOfView : MonoBehaviour
 {
-    public float MoveSpeed = 10f;
 
-
-    private Rigidbody _rb;
-    private Camera _cam;
-
-    private Vector3 _mousePosition;
-    private Vector3 _velocity = Vector3.zero;
-
-    private void Awake()
-    {
-        _rb = GetComponent<Rigidbody>();
-        _cam = Camera.main;
-    }
-
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        _mousePosition.x = Input.mousePosition.x;
-        _mousePosition.y = Input.mousePosition.y;
-        //z is the distance in case using perspective camera 
-        _mousePosition.z = _cam.transform.position.y;
-        Vector3 mousePos = _cam.ScreenToWorldPoint(_mousePosition);
-
-        transform.LookAt(mousePos + Vector3.up * transform.position.y);
-        _velocity.x = Input.GetAxisRaw("Horizontal");
-        _velocity.z = Input.GetAxisRaw("Vertical");
-        _velocity = _velocity.normalized * MoveSpeed;
-
-    }
-
-
-    void FixedUpdate()
-    {
-        _rb.MovePosition(_rb.position + _velocity * Time.fixedDeltaTime);
-    }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,21 @@
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float MoveSpeed = 10f;
+
+
+    private Rigidbody _rb;
+    private Camera _cam;
+
+    private Vector3 _mousePosition;
+    private Vector3 _velocity = Vector3.zero;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _cam = Camera.main;
+    }
+
     void Start()
     {
 
@@ -11,6 +25,22 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        _mousePosition.x = Input.mousePosition.x;
+        _mousePosition.y = Input.mousePosition.y;
+        //z is the distance in case using perspective camera 
+        _mousePosition.z = _cam.transform.position.y;
+        Vector3 mousePos = _cam.ScreenToWorldPoint(_mousePosition);
 
+        transform.LookAt(mousePos + Vector3.up * transform.position.y);
+        _velocity.x = Input.GetAxisRaw("Horizontal");
+        _velocity.z = Input.GetAxisRaw("Vertical");
+        _velocity = _velocity.normalized * MoveSpeed;
+
+    }
+
+
+    void FixedUpdate()
+    {
+        _rb.MovePosition(_rb.position + _velocity * Time.fixedDeltaTime);
     }
 }
